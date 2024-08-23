c47d2c4805703241fb4a8cff265b5790294fd4c3
Juliana0121 <juliananuma3@gmail.com>
2024-08-23 02:50:59 -0500

    feat: :sparkles: Se agregaron funciones para finalizar la compra
    Se divio el html de datos para que se vea mas ordenado, y se agregaron algunas funciones para que sea mas dinamica la pagina

---
+++ script/datos.js
@@ -0,0 +1,8 @@
+function showConfirmation() {
+    document.getElementById('confirmation-modal').style.display = 'flex';
+}
+
+function closeConfirmation() {
+    document.getElementById('confirmation-modal').style.display = 'none';
+    window.location.href = '../index.html'; 
+}
