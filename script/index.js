a86f95b91bffe600af730efb36370cacd2bd91d1
Juliana0121 <juliananuma3@gmail.com>
2024-08-23 04:56:42 -0500

    feat: :sparkles: Inicio de sesion
    Se realizo el inicio de sesión con loca storage y todo funcional

--- script/index.js
+++ script/index.js
@@ -1,12 +1,12 @@
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
-    window.location.href = 'login.html'
+    window.location.href = './pages/login.html'
 }
 
 const logout = document.querySelector('#logout')
 
 logout.addEventListener('click', ()=>{
-    alert('Hasta pronto!')
+    alert('Ingresando a la página web!')
     localStorage.removeItem('login_success')
-    window.location.href = 'login.html'
+    window.location.href = './pages/login.html'
 })
